1129d9851f8dbf3acae62a73d4b8405672cf3dae
Rimma_M <rimmafon@gmail.com>
2025-07-07 19:09:39 +0300

    collapse and expand all days at once

---
+++ app/src/main/java/com/github/cawboyroy/mywallet/main/presentation/AllCollapsedUi.kt
@@ -0,0 +1,58 @@
+package com.github.cawboyroy.mywallet.main.presentation
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.github.cawboyroy.mywallet.R
+import java.io.Serializable
+
+interface AllCollapsedUi : Serializable {
+
+    @Composable
+    fun Show(actions: AllDayActions)
+
+    abstract class Abstract(
+        @StringRes private val description: Int,
+        private val icon: ImageVector,
+    ) : AllCollapsedUi {
+
+        protected abstract fun onClick(actions: AllDayActions)
+
+        @Composable
+        override fun Show(actions: AllDayActions) = Button(
+            modifier = Modifier.padding(8.dp),
+            onClick = { onClick(actions) }
+        ) {
+            Icon(
+                icon,
+                contentDescription = stringResource(description)
+            )
+        }
+    }
+
+    data object Collapsed : Abstract(R.string.expand, Icons.Filled.KeyboardArrowDown) {
+        private fun readResolve(): Any = Collapsed
+        override fun onClick(actions: AllDayActions) = actions.expandAll()
+    }
+
+    data object Expanded : Abstract(R.string.collapse, Icons.Filled.KeyboardArrowUp) {
+        private fun readResolve(): Any = Collapsed
+        override fun onClick(actions: AllDayActions) = actions.collapseAll()
+    }
+}
+
+interface AllDayActions {
+
+    fun expandAll()
+
+    fun collapseAll()
+}
